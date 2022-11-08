2503cee5f64f0c9cdc12d3e7761e202effd580a3
b-sandrine <sanrdushimimana@gmail.com>
2022-11-08 21:54:16 +0200

    Finish adding props in the Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from './components/Card';
+import image from "./assets/katie-zaferes.png";
 
 function App() {
   return(
     <div className="container">
       <Navbar />
       <Hero />
-      <Card img='../assets/katie-zeferes.png' rating="5.0" reviewCount={6} country="USA" title="Life Lessons with ketie Zefares"
+      <Card img={image} rating="5.0" reviewCount={6} country="USA" title="Life Lessons with ketie Zefares"
       price={136} />
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ function Card(props) {
                     <img src={star} className="card--star" />
                     <span>{props.rating}</span>
                     <span className='gray'>({props.reviewCount}) • </span>
-                    <span className='gray'>{props.country}</span>
+                    <span className='gray'> {props.country}</span>
                 </div>
                 <p>{props.title}</p>
                 <p><span className='bold'>From ${props.price}</span> / person</p>
