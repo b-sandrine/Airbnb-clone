f7151bb5fa2e9293d78c66c347e4d31e7711d84d
b-sandrine <sanrdushimimana@gmail.com>
2022-11-08 21:47:28 +0200

    Add props in Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Card from './components/Card'
+import Card from './components/Card';
+
 function App() {
   return(
     <div className="container">
       <Navbar />
       <Hero />
-      <Card />
+      <Card img='../assets/katie-zeferes.png' rating="5.0" reviewCount={6} country="USA" title="Life Lessons with ketie Zefares"
+      price={136} />
     </div>
   )
 }
