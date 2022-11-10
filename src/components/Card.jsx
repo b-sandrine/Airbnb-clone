0bc83fe1b5bc122818b66ffda35531addf09f940
b-sandrine <sanrdushimimana@gmail.com>
2022-11-10 17:50:36 +0200

    Add ONLINE badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,18 @@
 import star from '../assets/star.png'
 
 function Card(props) {
+    let badgeText;
+    if(props.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    }
+    else if(props.country === "Online") {
+        badgeText = "ONLINE";
+    }
+
     return (
         <section>
             <div className="card">
-                {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+                {badgeText && <div className="card--badge">{badgeText}</div>}
                 <img src={props.img} className="card--image" />
                 <div className="card--stats">
                     <img src={star} className="card--star" />
